632c743e176321bd78eafb617a11c571f0a97260
Ellen Juhlin <ellenjuhlin@gmail.com>
2024-06-17 21:45:06 -0700

    Reliable serial output when vending starts and stops, with specific vending column and drawer.

--- VendyFriendy.ino
+++ VendyFriendy.ino
@@ -24,8 +24,8 @@ uint8_t sensorValue4 = 0;
 uint8_t sensorValue5 = 0;
 uint8_t sensorValue6 = 0;
 
-uint8_t drawers[3] = {sensorValueA, sensorValueB, sensorValueC};
-uint8_t columns[6] = {columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]};
+uint8_t drawers[3] = { sensorValueA, sensorValueB, sensorValueC };
+uint8_t columns[6] = { columns[0], columns[1], columns[2], columns[3], columns[4], columns[5] };
 
 int columnCounter = 0;
 int whichColumn = -1;
@@ -33,12 +33,17 @@ int prevColumn = -1;
 int drawerCounter = 0;
 int whichDrawer = -1;
 
+int VENDING_STATE = 0;
+
+unsigned long vendingTime = 0;
+unsigned long currentMillis = 0;
+const long interval = 2600;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(115200);
-  while (!Serial) delay(10); // Wait for console to open
-  
+  while (!Serial) delay(10);  // Wait for console to open
+
   Serial.println("Adafruit ADS7830 initializing - by Limor Fried/Ladyada");
 
   // Possible arguments to begin():
@@ -46,81 +51,112 @@ void setup() {
   // begin(0x49); // Uses I2C address 0x49 and Wire
   // begin(0x48, &Wire1); // Uses I2C address 0x48 and Wire1
 
-  if (!ad7830A.begin(0x48)) {    // SET I2C ADDRESS HERE
+  if (!ad7830A.begin(0x48)) {  // SET I2C ADDRESS HERE
     Serial.println("Failed to initialize ADS7830-A!");
-    while (1);
+    while (1)
+      ;
   }
-  if (!ad7830B.begin(0x49)) {    // SET I2C ADDRESS HERE
+  if (!ad7830B.begin(0x49)) {  // SET I2C ADDRESS HERE
     Serial.println("Failed to initialize ADS7830-B!");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("start Vendy Friendy...");
 }
 
 void loop() {
-  // read the analog in value:
-  drawers[0] = ad7830A.readADCsingle(aPin0);  // 400+ for vending drawer motor
-  drawers[1] = ad7830A.readADCsingle(aPin1);  // 400+ for vending drawer motor
-  drawers[2] = ad7830A.readADCsingle(aPin2);  // 400+ for vending drawer motor
-  columns[0] = ad7830A.readADCsingle(aPin3);  // 400+ for vending drawer motor
-  columns[1] = ad7830A.readADCsingle(aPin4);  // 400+ for vending drawer motor
-  columns[2] = ad7830A.readADCsingle(aPin5);  // 400+ for vending drawer motor
-  columns[3] = ad7830A.readADCsingle(aPin6);  // 400+ for vending drawer motor
-  columns[4] = ad7830A.readADCsingle(aPin7);  // 400+ for vending drawer motor
-  columns[5] = ad7830B.readADCsingle(bPin0);  // 400+ for vending drawer motor
+  switch (VENDING_STATE) {
+    case 0:
+      // read the analog value for the columns
+      columns[0] = ad7830A.readADCsingle(aPin3);
+      columns[1] = ad7830A.readADCsingle(aPin4);
+      columns[2] = ad7830A.readADCsingle(aPin5);
+      columns[3] = ad7830A.readADCsingle(aPin6);
+      columns[4] = ad7830A.readADCsingle(aPin7);
+      columns[5] = ad7830B.readADCsingle(bPin0);
 
-  whichColumn = columnTest();
-  if (whichColumn != -1) {
-    Serial.print("Maybe ");
-    Serial.println(whichColumn);
-    if (SameOrNewColumn()) {
-      if (columnCounter > 3) {
-        Serial.println("Test for which drawer");
+      // IMPORTANT VENDING DETECTION CODE HERE
+      whichColumn = columnTest();
+      if (whichColumn != -1) {
+        //Serial.print("Maybe column ");
+        //Serial.println(whichColumn+1);
+        if (SameOrNewColumn()) {
+          if (columnCounter > 2) {
+            //Serial.print(", testing for drawer, result: ");
+            whichDrawer = drawerTest();
+            if (whichDrawer == -1) {
+              //Serial.println("No drawer");
+            } else {
+              //Serial.println(whichDrawer);
+              confirmVending(whichColumn, whichDrawer);
+            }
+          } else {
+            //Serial.println(" maybe.");
+          }
+        } else {
+          //Serial.println(" nope.");
+        }
       }
-    }
+
+      delay(50);
+      break;
+
+    // If Vending is actively happening, check the time
+    case 1:
+      currentMillis = millis();
+      if (currentMillis - vendingTime >= interval) {
+        Serial.println("Vending time over!");
+        VENDING_STATE = 0;
+      } else {
+        delay(50);
+      }
+      break;
+    default:
+      VENDING_STATE = 0;
+      break;
   }
+      /*
+      // print the results to the Serial Monitor (uncomment this block for debugging)
+      drawers[0] = ad7830A.readADCsingle(aPin0);
+      drawers[1] = ad7830A.readADCsingle(aPin1);
+      drawers[2] = ad7830A.readADCsingle(aPin2);
+      Serial.print("sensors = ");
+      //Serial.print((sensorValueA - columns[0] > 130 && sensorValueA - columns[0] < 144) ? "1-GO\t" : "STOP\t");
+      //Serial.print((sensorValueA - columns[1] > 130 && sensorValueA - columns[1] < 144) ? "2-GO\t" : "STOP\t");
+      //Serial.print((sensorValueA - columns[2] > 130 && sensorValueA - columns[2] < 144) ? "3-GO\t" : "STOP\t");
+      Serial.print("A ");
+      Serial.print(drawers[0]);
+      Serial.print("\tB ");
+      Serial.print(drawers[1]);
+      Serial.print("\tC ");
+      Serial.print(drawers[2]);
+      Serial.print("\t1 ");
+      Serial.print(columns[0]);
+      Serial.print("\t2 ");
+      Serial.print(columns[1]);
+      Serial.print("\t3 ");
+      Serial.print(columns[2]);
+      Serial.print("\t4 ");
+      Serial.print(columns[3]);
+      Serial.print("\t5 ");
+      Serial.print(columns[4]);
+      Serial.print("\t6 ");
+      Serial.print(columns[5]);
+      Serial.print("\tCC ");
+      Serial.println(columnCounter);
 
-  // print the results to the Serial Monitor:
-  Serial.print("sensors = ");
-  //Serial.print((sensorValueA - columns[0] > 130 && sensorValueA - columns[0] < 144) ? "1-GO\t" : "STOP\t");
-  //Serial.print((sensorValueA - columns[1] > 130 && sensorValueA - columns[1] < 144) ? "2-GO\t" : "STOP\t");
-  //Serial.print((sensorValueA - columns[2] > 130 && sensorValueA - columns[2] < 144) ? "3-GO\t" : "STOP\t");
-  Serial.print("A ");
-  Serial.print(drawers[0]);
-  Serial.print("\tB ");
-  Serial.print(drawers[1]);
-  Serial.print("\tC ");
-  Serial.print(drawers[2]);
-  Serial.print("\t1 ");
-  Serial.print(columns[0]);
-  Serial.print("\t2 ");
-  Serial.print(columns[1]);
-  Serial.print("\t3 ");
-  Serial.print(columns[2]);
-  Serial.print("\t4 ");
-  Serial.print(columns[3]);
-  Serial.print("\t5 ");
-  Serial.print(columns[4]);
-  Serial.print("\t6 ");
-  Serial.print(columns[5]);
-  Serial.print("\tCC ");
-  Serial.println(columnCounter);
-
-  //Serial.print("\t output = ");
-  //Serial.println(outputValue);
-
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  delay(400);
+      //Serial.print("\t output = ");
+      //Serial.println(outputValue);
+    //*/
 }
 
 int columnTest() {
   for (int i = 0; i < 6; i++) {
-    if (columns[i] > 0 && columns[i] < 10) {  // define range of voltage on column while vending
+    if (columns[i] > 3 && columns[i] < 10) {  // define range of voltage on column while vending
       return i;                               // return the active column (if any)
     }
   }
-  columnCounter = 0;                          // otherwise reset the counters
+  columnCounter = 0;  // otherwise reset the counters
   prevColumn = -1;
   return -1;
 }
@@ -129,12 +165,10 @@ bool SameOrNewColumn() {
   if (whichColumn == prevColumn) {  // If a column has been in the range for a subsequent time
     columnCounter++;
     return true;
-  }
-  else if (prevColumn == -1) {      // If a column enters the range for the first time
+  } else if (prevColumn == -1) {  // If a column enters the range for the first time
     prevColumn = whichColumn;
     return true;
-  }
-  else {                            // If a column is in range but different from previous column
+  } else {  // If a column is in range but different from previous column
     prevColumn = whichColumn;
     whichColumn = -1;
     columnCounter = 0;
@@ -143,5 +177,34 @@ bool SameOrNewColumn() {
 }
 
 int drawerTest() {
+  drawers[0] = ad7830A.readADCsingle(aPin0);
+  drawers[1] = ad7830A.readADCsingle(aPin1);
+  drawers[2] = ad7830A.readADCsingle(aPin2);
+  for (int i = 0; i < 3; i++) {
+    if (drawers[i] > 130) {
+      return i;
+    }
+  }
   return -1;
+}
+
+void confirmVending(int c, int d) {
+  Serial.print("Vending! ");
+  vendingTime = millis();
+  VENDING_STATE = 1;
+  switch (d) {
+    case 0:
+      Serial.print("A-");
+      break;
+    case 1:
+      Serial.print("B-");
+      break;
+    case 2:
+      Serial.print("C-");
+      break;
+    default:
+      Serial.print("Bad drawer match - ");
+      break;
+  }
+  Serial.println(c + 1);
 }
